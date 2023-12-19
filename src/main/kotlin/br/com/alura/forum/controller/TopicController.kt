db92e7a3a1497de93c500caaea89994ef30ddffc
fifiuza <maicon.b.fiuza@gmail.com>
2023-12-19 20:52:06 -0300

    :spakles: adiciona método de buscar tópico por id

--- src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicController.kt
@@ -3,6 +3,7 @@ package br.com.alura.forum.controller
 import br.com.alura.forum.services.TopicService
 import br.com.alura.forum.entities.Topic
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -15,5 +16,10 @@ class TopicController(
     @GetMapping
     fun getTopic(): List<Topic> {
         return topicService.getTopic()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): Topic {
+        return topicService.getById(id)
     }
 }
--- src/main/kotlin/br/com/alura/forum/services/TopicService.kt
+++ src/main/kotlin/br/com/alura/forum/services/TopicService.kt
@@ -6,10 +6,11 @@ import br.com.alura.forum.entities.User
 import org.springframework.stereotype.Service
 
 @Service
-class TopicService {
-
-    fun getTopic(): List<Topic> {
-        return listOf(Topic(
+class TopicService(
+        private var topics: List<Topic>
+) {
+    init {
+       val topic1 =  Topic(
                 id = 1,
                 title = "Dúvida Kotlin",
                 message = "Variáveis no Kotlin",
@@ -23,6 +24,30 @@ class TopicService {
                         name = "Teste",
                         email = "teste@teste.com"
                 )
-        ))
+        )
+        val topic2 =  Topic(
+                id = 2,
+                title = "Dúvida Spring",
+                message = "Injeção de depêndencia",
+                course = Course(
+                        id = 2,
+                        name = "Spring Boot",
+                        category = "Programação",
+                ),
+                user = User(
+                        id = 2,
+                        name = "OutroTeste",
+                        email = "outroteste@teste.com"
+                )
+        )
+
+        topics = listOf(topic1, topic2)
+    }
+    fun getTopic(): List<Topic> {
+        return topics
+    }
+
+    fun getById(id: Long): Topic {
+        return topics.first { it.id == id }
     }
 }
