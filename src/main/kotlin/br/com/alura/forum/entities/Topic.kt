61fd9337d34f4f09027900d99322991f1b213c72
fifiuza <maicon.b.fiuza@gmail.com>
2024-02-06 20:33:04 -0300

    :sparkles: adiciona o mapeamento das entidades

--- src/main/kotlin/br/com/alura/forum/entities/Topic.kt
+++ src/main/kotlin/br/com/alura/forum/entities/Topic.kt
@@ -1,14 +1,21 @@
 package br.com.alura.forum.entities
 
+import jakarta.persistence.*
 import java.time.LocalDateTime
 
+@Entity
 data class Topic(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long? = null,
         val title: String,
         val message: String,
         val createdIn: LocalDateTime =  LocalDateTime.now(),
+        @ManyToOne
         val course: Course,
+        @ManyToOne
         val user: User,
+        @Enumerated(value = EnumType.STRING)
         val status: Status = Status.NOT_ANSWERED,
+        @OneToMany(mappedBy = "topic")
         val responses: List<Response> = emptyList(),
 )
